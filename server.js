55d6b01b92743a3091951d8e44cca106489169c5
= <=>
2025-10-21 12:02:07 +0300

    Refactor server.js to define express server and log melt version on startup

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const child_process = require("child_process");
 
-try {
-	child_process.execSync(`melt -v`);
-} catch (error) {
-	console.log(Buffer.from(error.output[1]).toString("utf-8"));
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Function to run 'melt -v' and log the output
+function logMeltVersion() {
+	try {
+		child_process.execSync(`melt -v`);
+	} catch (error) {
+		return Buffer.from(error.output[1]).toString("utf-8");
+	}
 }
+
+// Log melt version on server startup
+console.log(logMeltVersion());
+
+// Basic endpoint to verify the server is running
+app.get("/", (req, res) => {
+	res.send(logMeltVersion());
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
