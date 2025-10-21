419e151ea999ae78ab1fedcdb9bdddf06ed2c5e8
= <=>
2025-10-21 12:45:38 +0300

    write-read

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const child_process = require("child_process");
-
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const outputDir = path.join(__dirname, "outputs");
 
 // Function to run 'melt -v' and log the output
 function logMeltVersion() {
@@ -19,10 +21,19 @@ function logMeltVersion() {
 console.log("init log!!!", logMeltVersion());
 
 // Basic endpoint to verify the server is running
-app.get("/", (req, res) => {
-	console.log("request!!!!");
+app.get("/write", (req, res) => {
+	fs.mkdirSync(outputDir, { recursive: true });
+	const result = fs.writeFileSync(path.join(outputDir, "file.txt"), "input!!!");
 
-	res.send(logMeltVersion());
+	res.send({ result });
+});
+app.get("/read", (req, res) => {
+	const result = fs.readFileSync(path.join(outputDir, "file.txt"), {
+		encoding: "utf-8",
+	});
+	console.log({ result });
+
+	res.json({ result });
 });
 
 app.listen(port, () => {
