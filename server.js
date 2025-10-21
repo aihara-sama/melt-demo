ebc60ed0659a51392ca894235b510f5247b2a7f3
= <=>
2025-10-21 15:34:45 +0300

    13

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post("/upload", upload.single("video"), async (req, res) => {
 
 	try {
 		const result = child_process.execSync(
-			`melt -profile atsc_1080p_25 color:0 out=150 "${filePath}" in=0 out=150 in=0 out=150 -filter frei0r.select0r color=0x000000ff invert=1 subspace=0 shape=0.5 edge=0.9 delta_r=0.2 delta_g=0.2 delta_b=0.2 slope=0 operation=0.5 -transition mix a_track=0 b_track=1 always_active=1 sum=1 -transition qtblend a_track=0 b_track=2 always_active=1 -consumer avformat:/app/melt-demo/output/1.webm f=webm vcodec=libvpx-vp9 acodec=libvorbis vb=20M crf=15 pix_fmt=yuva420p aq=4 channels=2 threads=0 real_time=-1`
+			`melt -profile atsc_1080p_25 color:0 out=150 "${filePath}" in=0 out=150 in=0 out=150 -filter frei0r.select0r color=0x000000ff invert=1 subspace=0 shape=0.5 edge=0.9 delta_r=0.2 delta_g=0.2 delta_b=0.2 slope=0 operation=0.5 -transition mix a_track=0 b_track=1 always_active=1 sum=1 -transition qtblend a_track=0 b_track=2 always_active=1 -consumer avformat:/app/output/1.webm f=webm vcodec=libvpx-vp9 acodec=libvorbis vb=20M crf=15 pix_fmt=yuva420p aq=4 channels=2 threads=0 real_time=-1`
 		);
 		console.log({ result });
 		return res.json({
